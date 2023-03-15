fb47bce8872ba9364492bceedddca58adbc9db36
joshua bell <joshua@th3rdcoast.com>
2023-03-15 17:17:44 -0400

    update data and remove out of date details

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,10 +6,8 @@ export const About = () => {
     return <>
             <TabTitle title="About Me" />
             <p>I am a full stack software engineer with a passion for solving interesting problems
-                by designing and implementing software solutions across  software stacks.
+                by designing and implementing software solutions across software stacks.
             </p>
-            <p>Age ... 24</p>
-            <p>Residence ... Michigan</p>
             <div style={{display: 'flex'}}><p style={{marginRight: '5px'}}>Email ...  </p>{Email}</div>
         </>;
 }
