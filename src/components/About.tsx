e9637d07a4c9870f9fb02dc2da863d10ae41461b
josh bell <joshbel@proton.me>
2023-04-13 17:46:39 -0400

    Update resume and about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,10 +5,9 @@ import { Email } from '../Constants';
 export const About = () => {
     return <>
             <TabTitle title="About Me" />
-            <p>I am a full stack software engineer with a passion for solving interesting problems
-                by designing and implementing software solutions across software stacks. I love to explore
-                the intersections of art and software. I'm always looking for ways to make software more elegant
-                and more interesting.
+            <p>I'm a full stack software engineer with a passion for solving interesting problems
+                with software. I love to exploring the intersections of art and software. I'm always 
+                looking for ways to make software more elegant and more interesting.
             </p>
             <div style={{display: 'flex'}}><p style={{marginRight: '5px'}}>Email ...  </p>{Email}</div>
         </>;
